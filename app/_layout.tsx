eaf3b0b539d811d89ed395abd670c4afd422eb8c
Neerajan Dhakal <nirajandhakal634@gmail.com>
2025-09-04 19:20:28 +0545

    initial setup complete

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { SplashScreen, Stack } from "expo-router";
+import './global.css';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded, error] = useFonts({
+    Quicksand_400: require("../assets/fonts/Quicksand-Regular.ttf"),
+    Quicksand_500: require("../assets/fonts/Quicksand-Medium.ttf"),
+    Quicksand_600: require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    Quicksand_700: require("../assets/fonts/Quicksand-Bold.ttf"),
+    Quicksand_800: require("../assets/fonts/Quicksand-Light.ttf"),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  useEffect(() => {
+    if (error) throw error;
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error])
+  
+
+  return <Stack
+    screenOptions={{
+      headerShown: false,
+    }}
+  />;
 }
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="justify-center items-center flex-1 text-5xl font-quicksand-bold">
+      <Text className="text-blue-500">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
